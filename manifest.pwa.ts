76aa3c8fa8462d8598847dc14771f673a49fea27
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2023-06-21 02:36:25 -0300

    Add PWA settings

---
+++ manifest.pwa.ts
@@ -0,0 +1,25 @@
+
+export default {
+    "name": "Jardim Botânico",
+    "id": "/",
+    "short_name": "Jardim Botânico",
+    "start_url": ".",
+    "display": "standalone",
+    "background_color": "#fff",
+    "description": "Controle de vistas do Jardim Botânico / UFSM",
+    "icons": [
+        {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+        },{
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },{
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        },
+    ]
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from "vite-plugin-pwa";
+
+import manifest from './manifest.pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    VitePWA({
+      manifest,
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        sourcemap: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+      }
+    })
   ],
   resolve: {
     alias: {
